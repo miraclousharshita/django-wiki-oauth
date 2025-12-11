ba112bdcc4095728cbe9da57473c7ec71ab7b196
Harshita <harshita.21ug1034@iiitranchi.ac.in>
2025-12-11 14:17:35 +0530

    Update DATABSES settings

--- src/oauth_app/settings.py
+++ src/oauth_app/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'oauth_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 's57235__harshita_wiki_test',
+        'HOST': 'tools.db.svc.wikimedia.cloud',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'read_default_file': os.path.expanduser("~/replica.my.cnf")
+        },
     }
 }
 
